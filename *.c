184d51716c87e2246c7da361ab6f1da86bca0011
rajeshraj12345 <35956590+rajeshraj12345@users.noreply.github.com>
2018-02-24 11:31:42 +0530

    Create *.c

---
+++ *.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(i==1||j==1||i==5||j==i||i==2j||i==2j)
+            {
+                printf("*\t");
+            }
+            printf("\t");
+            
+            }
+            printf("*\n");
+        }
+}
