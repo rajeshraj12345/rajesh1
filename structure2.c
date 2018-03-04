c38bfded9951c7459875b3529925f08f52f02569
rajeshraj12345 <35956590+rajeshraj12345@users.noreply.github.com>
2018-03-04 14:52:25 +0530

    Create structure2.c

---
+++ structure2.c
@@ -0,0 +1,20 @@
+ 
+#include<stdio.h>
+struct stud 
+{
+    int id;
+    char name[10];
+    int m1,m2,m3;
+    int total;
+    int avg;
+} ;
+int main()
+{
+    struct stud s;
+    printf("\nenter the student details");
+    scanf("%d %s %d %d %d",&s.id,s.name,&s.m1,&s.m2,&s.m3,&s.total,&s.avg);
+    s.total=s.m1+s.m2+s.m3;
+    s.avg=(s.total/3);
+    printf("%d\n %s\n %d\n %d\n %d\n %d\n %d\n",s.id,s.name,s.m1,s.m2,s.m3,s.total,s.avg);
+    return 0;
+}
