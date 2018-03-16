667c8630e09b5aa1546eeb3c6c68ff8d97a6eef4
rajeshraj12345 <35956590+rajeshraj12345@users.noreply.github.com>
2018-03-16 04:07:41 -0700

    Create minium no.c

---
+++ minium no.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int a[4],i,min=a[0];
+    printf("enter the value");
+    scanf("%d",&a[i]);
+    for(i=0;i<=4;i++)
+    {
+scanf("%d",&a[i]);
+        
+    }
+    for(i=0;i<=4;i++)
+    {
+    if(a[i]<min)
+    {
+        min=a[i];
+    }
+    }
+    printf("%d",min);
+    
+        
+}  
